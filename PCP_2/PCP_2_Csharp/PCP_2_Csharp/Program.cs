f66305d400d661a3387e4917f3d8a4a8ca09b903
LisnychaNastia <94173940+LisnychaNastia@users.noreply.github.com>
2023-03-21 18:35:25 +0200

    init

---
+++ PCP_2/PCP_2_Csharp/PCP_2_Csharp/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PCP_2_Csharp
+{
+    internal class Program
+    {
+        private static readonly int dim = 20;
+        private static readonly int threadNum = 3;
+
+        private readonly Thread[] thread = new Thread[threadNum];
+
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+            main.InitArr();
+            main.Print();
+            Console.WriteLine(main.PartMin(0, dim));
+
+            Console.WriteLine(main.ParallelMin());
+            Console.ReadKey();
+        }
+
+        private void Print()
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private int threadCount = 0;
+
+        private long ParallelMin()
+        {
+            int step = dim / threadNum;
+            for (int i = 0; i < threadNum - 1; i++)
+            {
+                thread[i] = new Thread(StarterThread);
+                thread[i].Start(new Bound(i * step, (i + 1) * step));
+            }
+            thread[threadNum - 1] = new Thread(StarterThread);
+            thread[threadNum - 1].Start(new Bound((threadNum - 1) * step, dim));
+
+            lock (lockerForCount)
+            {
+                while (threadCount < threadNum)
+                {
+                    Monitor.Wait(lockerForCount);
+                }
+            }
+            return min;
+        }
+
+        private readonly int[] arr = new int[dim];
+
+        private void InitArr()
+        {
+            Random r = new Random();
+            for (int i = 0; i < dim; i++)
+            {
+                arr[i] = r.Next(-20, 20);
+            }
+        }
+
+        class Bound
+        {
+            public Bound(int startIndex, int finishIndex)
+            {
+                StartIndex = startIndex;
+                FinishIndex = finishIndex;
+            }
+
+            public int StartIndex { get; set; }
+            public int FinishIndex { get; set; }
+        }
+
+        private readonly object lockerForMin = new object();
+        private void StarterThread(object param)
+        {
+            if (param is Bound)
+            {
+                long min = PartMin((param as Bound).StartIndex, (param as Bound).FinishIndex);
+
+                lock (lockerForMin)
+                {
+                    CollectMin(min);
+                }
+                IncThreadCount();
+            }
+        }
+
+        private readonly object lockerForCount = new object();
+        private void IncThreadCount()
+        {
+            lock (lockerForCount)
+            {
+                threadCount++;
+                Monitor.Pulse(lockerForCount);
+            }
+        }
+
+        private long min = int.MaxValue;
+        public void CollectMin(long min)
+        {
+            if (min < this.min)
+                this.min = min;
+        }
+
+        public long PartMin(int startIndex, int finishIndex)
+        {
+            int min = int.MaxValue;
+            for (int i = startIndex; i < finishIndex; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            return min;
+        }
+    }
+}
---
+++ PCP_2/PCP_2_Java/src/ArrClass.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+
+public class ArrClass {
+    private final int dim;
+    private final int threadNum;
+    private final int[] arr;
+    private int threadCount = 0;
+
+    public ArrClass(int dim, int threadNum) {
+        this.dim = dim;
+        this.threadNum = threadNum;
+        arr = RandomInsert();
+        globalMin = arr[0];
+    }
+
+    public int minPart(int startIndex, int finishIndex){
+        int min = arr[startIndex];
+
+        for(int i = startIndex; i < finishIndex; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public int getThreadCount(){
+        return threadCount;
+    }
+
+    synchronized private int getMin(){
+        while(getThreadCount() < threadNum){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return globalMin;
+    }
+
+    synchronized public void incThreadCount(){
+        threadCount++;
+        notify();
+    }
+
+    private int globalMin;
+    synchronized public void collectMin(int min){
+        if(min < globalMin){
+            globalMin = min;
+        }
+    }
+
+    private int[] RandomInsert(){
+        Random r = new Random();
+        int[] arr = new int[dim];
+        for(int i = 0; i < dim; i++){
+            arr[i] = r.nextInt(-20,20);
+        }
+        return arr;
+    }
+
+    public int threadMin(){
+        ThreadMin[] threadMin = new ThreadMin[threadNum];
+        int step = dim / threadNum;
+
+        for(int i = 0; i < threadNum - 1; i++){
+            threadMin[i] = new ThreadMin(i * step, (i + 1) * step, this);
+            new Thread(threadMin[i]).start();
+        }
+        threadMin[threadNum - 1] = new ThreadMin((threadNum - 1) * step, dim - 1, this);
+        new Thread(threadMin[threadNum - 1]).start();
+        return getMin();
+    }
+
+    public void print(){
+        for(var item : arr){
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+}
