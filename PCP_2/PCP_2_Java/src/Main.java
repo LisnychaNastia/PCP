1a8084a3082b40aac37779bb929ad3ede75a381b
LisnychaNastia <94173940+LisnychaNastia@users.noreply.github.com>
2023-03-28 16:17:41 +0300

    Update Main.java

--- PCP_2/PCP_2_Java/src/Main.java
+++ PCP_2/PCP_2_Java/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
         int threadNum = 3;
         ArrClass arrClass = new ArrClass(dim, threadNum);
         arrClass.print();
-        System.out.println(arrClass.minPart(0, dim));
+
         int min = arrClass.threadMin();
-        System.out.println(min);
+        System.out.println("Мінімальним елементом серед масиву є: " + (min));
+        System.out.println("Мінімальне значення під індексом: " + arrClass.index_min(min));
     }
 }
