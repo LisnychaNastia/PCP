dda27aeeadf748a5ef46bb5219ebcf143f7eaf9f
LisnychaNastia <94173940+LisnychaNastia@users.noreply.github.com>
2023-04-09 15:47:17 +0300

    small fix

---
+++ PCP_3/PCP_3_CSharp/PCP_3_CSharp/PCP_3_CSharp/Fabricator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCP_3_CSharp
+{
+    internal class Fabricator
+    {
+    }
+}
